bbbfed785e3e11bac96a8d64a94bdf890ab7dcc8
DAGMAX83 <DAGMAX983@gmail.com>
2018-11-27 19:16:34 -0600

    Created js folders main and connections.

---
+++ bamazonCustomer.js
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS bamazonDB;
+
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(45) NULL,
+  department_name VARCHAR(45) NULL,
+  price DECIMAL(10,2) NULL,
+  stock_quantity INT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (poduct_name, department_name, price, stock_quantity)
+VALUES ( "apple watch", "tech", 300.00, 30 );
